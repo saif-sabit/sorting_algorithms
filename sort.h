e8b87b3ed776452fe97d55f7a033e56c1b54a242
saif-sabit <saifsabit@gmail.com>
2023-07-29 18:51:14 +0300

    Betty

--- sort.h
+++ sort.h
@@ -12,12 +12,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-void swap(int* val1, int* val2);
+void swap(int *val1, int *val2);
 #endif
