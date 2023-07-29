0742a6f630b8c0cec1163b823257c824520295fb
saif-sabit <saifsabit@gmail.com>
2023-07-29 19:38:18 +0300

    Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,27 +2,33 @@
 /**
  * insertion_sort_list - Sorts array of int
  * @list: Array to be sorted
-*/
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-    current = (*list)->next;
-    while(current != NULL)
-    {
-        temp = current->prev;
-        while(temp !=NULL && temp->n > current->n)
-        {
-            swap_nodes(list,temp,current);
-            
-            temp = current->prev;
-            print_list(*list);
-        }
-        current = current->next;
-    }
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->prev;
+		while (temp != NULL && temp->n > current->n)
+		{
+			swap_nodes(list, temp, current);
+
+			temp = current->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
+/**
+ * swap_nodes - Swaps two nodes in list
+ * @list: list
+ * @node1: pointer to first node
+ * @node2: pointer to second node
+ */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
 	if (node1->prev != NULL)
