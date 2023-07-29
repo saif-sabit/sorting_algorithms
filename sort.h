8e987e386efccb72a0a02f0fc1bd6866d432f76e
asimsharf <asimsharf@gmail.com>
2023-07-30 01:23:36 +0300

    Implement cocktail sort algorithm in listint_t doubly-linked list.

--- sort.h
+++ sort.h
@@ -25,4 +25,6 @@ void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
 #endif
