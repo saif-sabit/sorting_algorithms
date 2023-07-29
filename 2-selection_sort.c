babcb1d10bec8066c905f9e66ec0db7e9a51df4a
saif-sabit <saifsabit@gmail.com>
2023-07-29 20:30:44 +0300

    Betty Again

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,23 +3,25 @@
  * selection_sort - Sorts array of int
  * @array: Array to be sorted
  * @size: Size of the array
-*/
+ */
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i, j, min_idx;
- 
-    for (i = 0; i < size - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < size; j++) {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            swap(&array[min_idx], &array[i]);
-            print_array(array, size);
-        }
-    }
+	unsigned int i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
 /**
  * swap - swaps two numbers
